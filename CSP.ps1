35762fe0109720777b92289c9b6be127d829cc47
VBV1 <104235290+VBV11@users.noreply.github.com>
2024-02-24 19:29:17 +0100

    Update CSP.ps1

--- CSP.ps1
+++ CSP.ps1
@@ -19,7 +19,7 @@ function Set-CameraRollPath {
 }
 
 # Nieuw pad voor de camerarol (verander dit naar jouw gewenste locatie)
-$newCameraRollPath = "$env:userprofile\Pictures\Camera Roll"
+$newCameraRollPath = "$env:userprofile\Pictures\CameraRoll"
 
 # Controleer of het nieuwe pad bestaat, zo niet, maak het dan aan
 if (-not (Test-Path -Path $newCameraRollPath)) {
